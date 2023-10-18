e366be056c22cd3de61710bd1ee1263135891fc0
LakshmiRamalingam <keerthiram1104@gmail.com>
2023-10-18 22:25:23 +0530

    jk

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 onclick="document.getElementById('demo').innerHTML = Date()">
 Click me to display Date and Time.
 <p id="demo"></p>
+onclick="document.getElementById"('keerthi').innerHTML="first program"
+<p id ="keerthi"></p>
 </button>
-
-
