393c4917e2724ce34bbe7bcfdf4b269633ae3252
LakshmiRamalingam <104923036+LakshmiRamalingam@users.noreply.github.com>
2023-11-08 22:24:28 +0530

    Update script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.
-<p id="demo"></p>
-onclick="document.getElementById"('keerthi').innerHTML="first program"
-<p id ="keerthi"></p>
-</button>
